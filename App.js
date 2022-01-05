bf6c867b50745ff68acbcf405450f5ea58fdb2bf
Sumit <sumit.webnware@gmail.com>
2022-01-05 20:01:27 +0530

    Test

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>this is testing Hello</Text>
+      <Text>this is testing</Text>
+      <Button
+        title="Go to Profile"
+        onPress={() => this.props.navigation.navigate('Login')}
+      />
       <StatusBar style="auto" />
     </View>
   );
